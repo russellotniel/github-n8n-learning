9a02b3e2818fbb991f84fe779382469c618a1dda
Russell Otniel <russellotniel@gmail.com>
2025-08-27 11:14:23 +0700

    feat: coba naikin ke 0.6.5-beta.1

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -30,7 +30,7 @@ export default async function ProtectedPage() {
       <div>
         <h2 className="font-bold text-2xl mb-4">Next steps</h2>
         <FetchDataSteps />
-        Coba naik ke 0.6.5-beta.0
+        Coba naik ke 0.6.5-beta.1 pake feat.
       </div>
     </div>
   );
