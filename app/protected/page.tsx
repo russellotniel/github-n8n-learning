3199e557915c0cb85f59fa3fb9d587e07cae18d8
Russell Otniel <58100565+russellotniel@users.noreply.github.com>
2025-09-02 08:37:07 +0700

    fix: fix ke 0.9.3-beta.0 (#55)

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -30,7 +30,7 @@ export default async function ProtectedPage() {
       <div>
         <h2 className="font-bold text-2xl mb-4">Next steps</h2>
         <FetchDataSteps />
-        Coba pake feat untuk naikin beta ke 0.9.2-beta.1
+        Coba pake feat untuk naikin beta ke 0.9.3-beta.0
       </div>
     </div>
   );
